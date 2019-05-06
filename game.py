7750072894f4bba1368e00e477ba200c83023629
EmreUygur <44411283+EmreUygur@users.noreply.github.com>
2019-05-07 00:19:17 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,47 @@
+import os
+import random
+import numpy as np
+from time import sleep
+
+def select_difficulty():
+  while 1:
+    print(" Zorluk Belirleyiniz")
+    print("     1) 4 ships, 4x4 map")
+    print("     2) 6 ships, 6x6 map")
+    print("     3) 8 ships, 8x8 map")
+    difficulty = input(" Choose Difficulty (1-3): ")
+    
+    try:
+      difficulty = int(difficulty)
+      
+      if(difficulty < 1 or difficulty >3):
+        os.system("cls")
+        print("#"*40,"BATTLESHIPS GAME","#"*40 )
+        print()
+        print(" Invalid Input !! Try Again...")
+        
+      else:
+        os.system("cls")
+        print("#"*40,"BATTLESHIPS GAME","#"*40 )
+        print()
+        return difficulty
+        
+    except:
+      os.system("cls")
+      print("#"*40,"BATTLESHIPS GAME","#"*40 )
+      print()
+      print(" Invalid Input !! Try Again...")
+
+def cpu_map(numOfShips):
+  map = np.zeros((numOfShips, numOfShips))
+  
+  for i in range(numOfShips):
+    x = random.randint(0,numOfShips-1)
+    y = random.randint(0,numOfShips-1)
+    
+    if(map[x][y] == 0):
+      map[x][y] = 1
+      break
+      
+  return map
+
